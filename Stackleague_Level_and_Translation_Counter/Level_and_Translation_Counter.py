fdb8ca89271916a7e54400d639717e53fb810af1
xjex <dev.xjmanaloto@gmail.com>
2023-11-16 02:59:41 +0800

    added save title and confirmation

--- Stackleague_Level_and_Translation_Counter/Level_and_Translation_Counter.py
+++ Stackleague_Level_and_Translation_Counter/Level_and_Translation_Counter.py
@@ -13,7 +13,7 @@ class Colors:
     WHITE = '\033[97m'
 
 # Replace 'your_file.xlsx' with the actual filename of your spreadsheet
-file_path = 'dir.xlsx'
+file_path = "./Stackleague_Level_and_Translation_Counter/dir2.xlsx"
 
 # Install openpyxl if not already installed
 try:
@@ -60,13 +60,36 @@ while user_input.lower() not in ('y', 'n'):
 
 if user_input.lower() == 'y':
     # Write the log to a text file
-    log_file_path = 'Log.txt'
-    with open(log_file_path, 'a') as log_file:
-        log_file.write(log_content)
-    os.system('cls' if os.name == 'nt' else 'clear')    
-    print(Colors.GREEN)
-    print(f"Log written to {log_file_path}")
-    print(Colors.RESET)
+    log_file_path = 'Stackleague_Log_Report.txt'
+
+     # Step 1: Enter name
+    file_name = input("Enter Log Title name: ")
+    save_log = False
+    while True:
+       
+        
+        # Step 2: Validate the name
+        validation = input (f"is {Colors.GREEN}{file_name}{Colors.WHITE} correct? (y/n)").lower()
+
+        if validation == 'y':
+            log_content_with_name = f"-------------------\n{file_name}\nLevel Counts:\n{levels_count.to_string()}\nComplete Translation: {complete_translation_count}\nTotal Contribution: {len(title)}\n-------------------\n"
+            with open(log_file_path, 'a') as log_file:
+                log_file.write(log_content_with_name)
+            os.system('cls' if os.name == 'nt' else 'clear')    
+            print(Colors.GREEN)
+            print(f"Log written to {log_file_path}")
+            print(Colors.RESET)
+            save_log = True
+            break  # Exit the loop if the name is correct
+
+        elif validation == 'n':
+           
+            file_name = input("Please Rename: ")
+        else:
+            
+            print("Invalid input. Please enter 'y' or 'n'.")
+
+
 else:
     os.system('cls' if os.name == 'nt' else 'clear')
     print(Colors.RED)
